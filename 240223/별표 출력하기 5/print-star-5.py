229035b3cce11974e13b860f1a92ed11a7d91735
AngelPlayer <ssh5212@naver.com>
2024-02-23 12:25:37 +0900

    Title: 별표 출력하기 5, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240223/별표 출력하기 5/print-star-5.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n - 1, -1, -1):
+    for _ in range(i + 1):
+        for j in range(i + 1):
+            print("*", end="")
+        print(" ", end="")
+    print()
