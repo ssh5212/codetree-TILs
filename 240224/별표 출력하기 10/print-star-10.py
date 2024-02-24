5a0561eac798e35b6f61101dbb9bc677cc7ca923
AngelPlayer <ssh5212@naver.com>
2024-02-24 13:18:14 +0900

    Title: 별표 출력하기 10, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240224/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(n):
+    if i % 2 == 0:
+        for j in range((i//2) + 1):
+            print("*", end=" ")
+        print()
+    if i % 2 == 1:
+        for j in range(n - (i//2)):
+            print("*", end=" ")
+        print()
+for i in range(n - 1, -1, -1):
+    if i % 2 == 0:
+        for j in range((i//2) + 1):
+            print("*", end=" ")
+        print()
+    if i % 2 == 1:
+        for j in range(n - (i//2)):
+            print("*", end=" ")
+        print()
