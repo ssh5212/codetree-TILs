05c42fba94297547940e73443886bc2fa3deead5
AngelPlayer <ssh5212@naver.com>
2024-03-05 12:52:47 +0900

    Title: 십의 자리 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240305/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,14 @@
+num_list = list(map(int, input().split()))
+
+arr = [0] * 10
+
+for i in num_list:
+    if i == 0:
+        break
+
+    now_num = num_list // 10
+
+    arr[now_num] += 1
+
+for i in range(1, 10):
+    print(f'{i} - {arr[i]}')
