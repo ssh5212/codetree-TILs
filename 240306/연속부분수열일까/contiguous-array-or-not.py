abbaeabb90b1016b0cf8eb43ad265570825d641f
AngelPlayer <ssh5212@naver.com>
2024-03-06 10:43:57 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240306/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,16 @@
+n1, n2 = map(int, input().split())
+
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+ans = 'No'
+
+for i in range(len(arr1)):
+    if arr1[i] == arr2[0]:
+        for j in range(len(arr2)):
+            if arr1[i + j] != arr2[j]:
+                break
+            if j == len(arr2) - 1:
+                ans = 'Yes'
+
+print(ans)
