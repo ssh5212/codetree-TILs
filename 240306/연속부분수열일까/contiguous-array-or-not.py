2fe18fc3158cf77d41da0dde17de40750ad47f89
AngelPlayer <ssh5212@naver.com>
2024-03-06 10:46:14 +0900

    Title: 연속부분수열일까, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/연속부분수열일까/contiguous-array-or-not.py
+++ 240306/연속부분수열일까/contiguous-array-or-not.py
@@ -6,6 +6,8 @@ arr2 = list(map(int, input().split()))
 ans = 'No'
 
 for i in range(len(arr1)):
+    if i + len(arr2) - 1 > len(arr1) - 1:
+        break
     if arr1[i] == arr2[0]:
         for j in range(len(arr2)):
             if arr1[i + j] != arr2[j]:
